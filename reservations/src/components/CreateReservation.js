1dbbee3cc2bc318b579c7bad42684320a2507a94
Irina Saf <132776531+rabbitrunout@users.noreply.github.com>
2025-08-20 11:32:00 -0400

    completed server api

---
+++ reservations/src/components/CreateReservation.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+function CreateReservation() {
+
+    return(
+        <div className="container mt-4">
+            <h2> Create a New Reservation</h2>
+        </div>
+    );
+}
+
+export default CreateReservation;
